5aec57843bdb20a04230fdf055cd6bf06cfcdb8e
Seaoftrees <tasuzuya@gmail.com>
2017-02-12 17:56:09 +0900

    Add files via upload

---
+++ primeNumbers/primeNumbers/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace primeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(278, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "素数！";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(139, 81);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1569325056,
+            23283064,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(407, 26);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "調べる";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "入力した数字が素数かどうかを調べます。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(164, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "結果: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 381);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "素数！";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ primeNumbers/primeNumbers/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //調べるボタンをクリックしたとき
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //入力された数字を取得
+            ulong subject = (ulong)numericUpDown1.Value;
+
+            //素数かどうかの判別
+            Boolean notPrimeNumber = false;
+
+            //ある数字で割り切れるときの値
+            ulong pp = 0;
+
+            //入力された値が2で割り切れないとき
+            if (subject % 2 !=0)
+            {
+                //素数かどうか調べる 3, 5, 7....という風に割っていく(ただし、割る数字は入力された数字の平方根より少ない)
+                for (ulong p = 3; p <= Math.Sqrt(subject); p += 2)
+                {
+                    //(入力された数字)÷(割る数)の余りが0のとき (つまり、割り切れるとき)
+                    if (subject % p == 0)
+                    {
+                        //notPrimeNumberにtrueを代入して、処理を中断する
+                        pp = p;
+                        notPrimeNumber = true;
+                        break;
+                    }
+                }
+            }
+            else //2で割り切れるとき
+            {
+                pp = 2;
+                notPrimeNumber = true;
+            }
+
+            //もし、for処理の中で割り切れる数字があったとき(=notPrimeNumberにtrueが代入されているとき)
+            if (notPrimeNumber)
+            {
+                //つまり、素数でないとき
+                label3.Text = "結果: 入力された数字は「素数ではありません！」( " + pp.ToString() + " で割り切れます)";
+            }
+            else if(subject == 1)   //そうでない場合に入力された数字が 1 のとき
+            {
+                label3.Text = "結果: 入力された数字は「素数ではありません！」(1はそもそも素数じゃないよ)";
+            }
+            else  //上のどれでもないとき (=素数のとき)
+            {
+                label3.Text = "結果: 入力された数字は「素数です！」";
+            }
+        }
+    }
+}
